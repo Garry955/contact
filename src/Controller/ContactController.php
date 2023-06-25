c1000447c297fb21e19641bd08294533d7ead700
Marosy Gergo <garry955@gmail.com>
2023-06-25 13:55:13 +0200

    set up form handling @ contact controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,36 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactFormType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
-    #[Route('/',methods:['GET','POST','HEAD'], name: 'contact')]
-    public function index(): Response
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    #[Route('/', methods: ['GET', 'POST', 'HEAD'], name: 'contact')]
+    public function index(Request $request): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newContact = $form->getData();
+
+            $this->em->persist($newContact);
+            $this->em->flush();
+
+            return $this->redirectToRoute('contact');
+        }
 
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView()
