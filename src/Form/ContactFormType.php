9341e2e9a4d75bfd07dd8fd2e1ee68f3bcef68ee
Marosy Gergo <garry955@gmail.com>
2023-06-25 14:36:05 +0200

    disable browser validation at ContactFormType

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -6,16 +6,36 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('text')
-        ;
+            ->add('name', TextType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => 'Név',
+                ),
+                'required' => false,
+            ])
+            ->add('email', TextType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => 'E-mail cím',
+                ),
+                'required' => false,
+            ])
+            ->add('text', TextareaType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => 'Üzenet szövege',
+                ),
+                'required' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
